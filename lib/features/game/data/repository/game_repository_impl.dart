0cd71a94c3b838850f06863e82a28f9480056387
christian bondzie <christian.zap@hotmail.fr>
2025-05-11 17:52:23 +0200

    Implementation du game repository.

--- lib/features/game/data/repository/game_repository_impl.dart
+++ lib/features/game/data/repository/game_repository_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:seven_days/core/errors/exceptions.dart';
 import 'package:seven_days/core/errors/failures.dart';
 import 'package:seven_days/features/game/data/datasources/game_remote_data_source.dart';
 import 'package:seven_days/features/game/domain/entity/game.dart';
@@ -11,20 +12,33 @@ class GameRepositoryImpl implements GameRepository {
   GameRepositoryImpl({required this.dataSource});
 
   @override
-  Future<Either<Failure, Game?>> activeGame() {
-    // TODO: implement activeGame
-    throw UnimplementedError();
+  Future<Either<Failure, Game?>> activeGame() async {
+    try {
+      final Game? game = await dataSource.activeGame();
+      return Right(game);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(errorMessage: failure.errorMessage));
+    }
   }
 
   @override
-  Future<Either<Failure, Game>> createGame({required List<Player> players}) {
-    // TODO: implement createGame
-    throw UnimplementedError();
+  Future<Either<Failure, Game>> createGame(
+      {required List<Player> players}) async {
+    try {
+      final Game game = await dataSource.createGame(players: players);
+      return Right(game);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(errorMessage: failure.errorMessage));
+    }
   }
 
   @override
-  Future<Either<Failure, Game>> updateGame({required int gameId}) {
-    // TODO: implement updateGame
-    throw UnimplementedError();
+  Future<Either<Failure, Game>> updateGame({required int gameId}) async {
+    try {
+      final Game game = await dataSource.updateGame(gameId: gameId);
+      return Right(game);
+    } on ServerException catch (failure) {
+      return Left(ServerFailure(errorMessage: failure.errorMessage));
+    }
   }
 }
