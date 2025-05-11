5ca29f6519dcb0c9820ffb009583cd4aac244db8
christian bondzie <christian.zap@hotmail.fr>
2025-05-11 15:38:32 +0200

    Creation de l abstraction du repository

---
+++ lib/features/game/domain/game_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:seven_days/core/errors/failures.dart';
+import 'package:seven_days/features/game/domain/entity/game.dart';
+import 'package:seven_days/features/player/domain/entity/player.dart';
+
+abstract class GameRepository {
+  Future<Either<Failure, Game>> createGame({required List<Player> players});
+  Future<Either<Failure, Game>> updateGame({required int gameId});
+  Future<Either<Failure, Game>> activeGame();
+}
