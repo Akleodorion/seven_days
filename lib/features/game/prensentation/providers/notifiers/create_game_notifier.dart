062a5afc04565892498d2d5425e00fa60080980b
christian bondzie <christian.zap@hotmail.fr>
2025-05-12 21:48:23 +0200

    Creation du notifier ajout de riverpod

---
+++ lib/features/game/prensentation/providers/notifiers/create_game_notifier.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seven_days/features/game/prensentation/providers/states/create_game_state.dart';
+
+class CreateGameNotifier extends StateNotifier<CreateGameState> {
+  CreateGameNotifier(super.state);
+}
