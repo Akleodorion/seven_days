e57e00b996e1732f9ed34eb04da100d29c87b606
christian bondzie <christian.zap@hotmail.fr>
2025-05-13 09:56:23 +0200

    Creation du update game notifier

---
+++ lib/features/game/prensentation/providers/notifiers/update_game_notifier.dart
@@ -0,0 +1,28 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seven_days/core/errors/failures.dart';
+import 'package:seven_days/features/game/domain/entity/game.dart';
+import 'package:seven_days/features/game/domain/usecases/update_game_usecase.dart';
+import 'package:seven_days/features/game/prensentation/providers/states/update_game_state.dart';
+
+class UpdateGameNotifier extends StateNotifier<UpdateGameState> {
+  final UpdateGameUsecase updateGameUsecase;
+
+  UpdateGameNotifier({required this.updateGameUsecase}) : super(Unloaded());
+
+  Future<UpdateGameState> updateGame({required int gameId}) async {
+    state = Loading();
+
+    final Either<Failure, Game> response =
+        await updateGameUsecase.call(gameId: gameId);
+
+    response.fold((failure) {
+      if (failure is ServerFailure) {
+        state = Error(errorMessage: failure.errorMessage, game: null);
+      }
+    }, (game) {
+      state = Loaded(game: game);
+    });
+    return state;
+  }
+}
