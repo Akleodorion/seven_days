b5239f6ab2e76df5b0d2dee4dd9e012c1efb927b
christian bondzie <christian.zap@hotmail.fr>
2025-05-13 09:01:49 +0200

    Creation du create game notifier

--- lib/features/game/prensentation/providers/notifiers/create_game_notifier.dart
+++ lib/features/game/prensentation/providers/notifiers/create_game_notifier.dart
@@ -1,6 +1,34 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:seven_days/core/errors/failures.dart';
+import 'package:seven_days/features/game/domain/entity/game.dart';
+import 'package:seven_days/features/game/domain/usecases/create_game_usecase.dart';
 import 'package:seven_days/features/game/prensentation/providers/states/create_game_state.dart';
+import 'package:seven_days/features/player/domain/entity/player.dart';
 
 class CreateGameNotifier extends StateNotifier<CreateGameState> {
-  CreateGameNotifier(super.state);
+  final CreateGameUsecase createGameUsecase;
+
+  CreateGameNotifier({
+    required this.createGameUsecase,
+  }) : super(Unloaded());
+
+  // Usecase
+
+  Future<CreateGameState> createGame({required List<Player> players}) async {
+    state = Loading();
+
+    final Either<Failure, Game> response =
+        await createGameUsecase.call(players: players);
+
+    response.fold((failure) {
+      if (failure is ServerFailure) {
+        state = Error(errorMessage: failure.errorMessage, game: null);
+      }
+    }, (game) {
+      state = Loaded(game: game);
+    });
+
+    return state;
+  }
 }
